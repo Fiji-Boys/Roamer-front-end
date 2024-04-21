147cbc28c8e773d4044e8eb8b757922ae58b0ddd
Marko Čekerinac <marko.cekerinac@gmail.com>
2024-04-21 17:12:42 +0200

    starter tour

--- lib/services/tour_service.dart
+++ lib/services/tour_service.dart
@@ -5,6 +5,56 @@ class TourService {
   List<Tour> getAll() {
     List<Tour> tours = [];
 
+    List<KeyPoint> starterTourKeyPoints = [
+      KeyPoint(
+        id: 1,
+        name: "Petrovaradin Fortress",
+        description:
+            "Petrovaradin Fortress nicknamed Gibraltar on/of the Danube, is a fortress in the town of Petrovaradin.",
+        images: [
+          "https://www.creativehistorybalkans.com/wp-content/uploads/2019/05/Novi_Sad_00_web_feat.jpg",
+          "https://nsuzivo.rs/wp-content/uploads/2021/10/Petrovaradinska_tvrdjava_2.jpg"
+        ],
+        latitude: 45.25214653312344,
+        longitude: 19.865811402657357,
+      ),
+      KeyPoint(
+        id: 2,
+        name: "Dunavski Park",
+        description:
+            "Danube Park or Dunavski Park is an urban park in the downtown of Novi Sad formed in 1895.",
+        images: [
+          "https://live.staticflickr.com/695/20996605404_dd50a482a7_b.jpg",
+          "https://www.shutterstock.com/image-photo/novi-sad-serbia-october-29-600nw-2071913192.jpg"
+        ],
+        latitude: 45.25529047021965,
+        longitude: 19.851207525802778,
+      ),
+      KeyPoint(
+        id: 3,
+        name: "The Statue of Jovan Jovanovic Zmaj",
+        description:
+            "The monument is built of bronze and is 230 centimeters high. The work is the work of sculptor Dragan Nikolić from Belgrade.",
+        images: [
+          "https://live.staticflickr.com/3835/14254427657_96a8d0b8fc_b.jpg"
+        ],
+        latitude: 45.25689538552521,
+        longitude: 19.847889666499356,
+      ),
+      KeyPoint(
+        id: 4,
+        name: "Name of Mary Church",
+        description:
+            "Roman Catholic parish church dedicated to the feast of the Holy Name of Mary.",
+        images: [
+          "https://novisad.travel/wp-content/uploads/2019/01/crkva-Imena-Marijinog-23_compressed.jpg",
+          "https://visitdistrikt.rs/media/2021/04/Visit-NS-Trg-slobode-foto-Jelena-Ivanovic-26-scaled.jpg"
+        ],
+        latitude: 45.255399209538496,
+        longitude: 19.845446657629196,
+      ),
+    ];
+
     List<KeyPoint> keyPoints = [
       KeyPoint(
           id: 1,
@@ -135,10 +185,16 @@ class TourService {
         description: "Easter egg tura",
         keyPoints: keyPoints4,
         type: TourType.adventure);
+    Tour starterTour = Tour(
+        name: "Starter tour",
+        description: "Explore Novi Sad",
+        keyPoints: starterTourKeyPoints,
+        type: TourType.informational);
     tours.add(newTour);
     tours.add(newTour2);
     tours.add(newTour3);
     tours.add(newTour4);
+    tours.add(starterTour);
 
     return tours;
   }
