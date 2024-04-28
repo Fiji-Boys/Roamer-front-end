81bb20620952c9b837c8767bc085e5506d3dd96a
gagi155 <maslac.ra151.2020@gmail.com>
2024-04-28 13:56:58 +0200

    changed headder adn image slider

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -551,7 +551,7 @@ class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin {
             2;
     double distance = 1000 * 12742 * asin(sqrt(a));
     debugPrint(distance.toString());
-    return distance < 100;
+    return distance < 50000;
   }
 
   void deleteKeyPoint(String currentKeyPoint) async {
