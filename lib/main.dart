986935e067c30f21449a258471de623671163211
Filip Simic <fsimic346@gmail.com>
2024-07-20 14:16:35 +0200

    Added firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
 import 'package:figenie/consts.dart';
+import 'package:figenie/firebase_options.dart';
 import 'package:figenie/pages/osmap/osmap_controller.dart';
 import 'package:figenie/pages/tours/tours_controller.dart';
 import 'package:figenie/widgets/placeholder.dart' as roamer_placeholder;
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
 import 'utils/navigation_menu.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MainApp());
 }
 
@@ -36,51 +45,102 @@ class MainApp extends StatelessWidget {
         ),
         primaryColor: primaryColor,
       ),
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        bottomNavigationBar: Obx(() {
-          return controller.isNavBarVisible.value
-              ? NavigationMenu(
-                  destinations: const [
-                      NavigationDestination(
-                        icon: Icon(Icons.tour),
-                        label: 'Tours',
-                      ),
-                      NavigationDestination(
-                        icon: Icon(Icons.people),
-                        label: 'Encounters',
-                      ),
-                      NavigationDestination(
-                        icon: Icon(Icons.map),
-                        label: 'Map',
-                      ),
-                      NavigationDestination(
-                        icon: Icon(Icons.qr_code),
-                        label: 'QR',
-                      ),
-                      NavigationDestination(
-                        icon: Icon(Icons.person),
-                        label: 'Profile',
-                      ),
-                    ],
-                  selectedIndex: controller.selectedIndex.value,
-                  onDestinationSelected: (index) => {
-                        _onNavigate(index),
-                        controller.selectedIndex.value = index
-                      })
-              : const SizedBox.shrink();
-        }),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0.0),
-            child: PageView(
-              controller: pageController,
-              physics: const NeverScrollableScrollPhysics(),
-              children: controller.screens,
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? "/sign-in" : "/home",
+      routes: {
+        "/sign-in": (context) {
+          return Container(
+            color: backgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/roamer_logo.png',
+                  height: 200,
+                  width: 200,
+                ),
+                const SizedBox(height: 40),
+                SignInButton(
+                  Buttons.google,
+                  text: "Sign in with Google",
+                  onPressed: () async {
+                    try {
+                      final googleProvider = GoogleAuthProvider();
+                      await FirebaseAuth.instance
+                          .signInWithProvider(googleProvider);
+                      Navigator.pushNamed(context, "/home");
+                    } catch (e) {
+                      print('Google sign-in failed: $e');
+                    }
+                  },
+                ),
+                const SizedBox(height: 20),
+                SignInButton(
+                  Buttons.gitHub,
+                  text: "Sign in with GitHub",
+                  onPressed: () async {
+                    try {
+                      final githubProvider = GithubAuthProvider();
+                      await FirebaseAuth.instance
+                          .signInWithProvider(githubProvider);
+                      Navigator.pushNamed(context, "/home");
+                    } catch (e) {
+                      print('GitHub sign-in failed: $e');
+                    }
+                  },
+                ),
+              ],
             ),
-          ),
-        ),
-      ),
+          );
+        },
+        "/home": (context) {
+          return Scaffold(
+            backgroundColor: Colors.black,
+            bottomNavigationBar: Obx(() {
+              return controller.isNavBarVisible.value
+                  ? NavigationMenu(
+                      destinations: const [
+                          NavigationDestination(
+                            icon: Icon(Icons.tour),
+                            label: 'Tours',
+                          ),
+                          NavigationDestination(
+                            icon: Icon(Icons.people),
+                            label: 'Encounters',
+                          ),
+                          NavigationDestination(
+                            icon: Icon(Icons.map),
+                            label: 'Map',
+                          ),
+                          NavigationDestination(
+                            icon: Icon(Icons.qr_code),
+                            label: 'QR',
+                          ),
+                          NavigationDestination(
+                            icon: Icon(Icons.person),
+                            label: 'Profile',
+                          ),
+                        ],
+                      selectedIndex: controller.selectedIndex.value,
+                      onDestinationSelected: (index) => {
+                            _onNavigate(index),
+                            controller.selectedIndex.value = index
+                          })
+                  : const SizedBox.shrink();
+            }),
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                child: PageView(
+                  controller: pageController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: controller.screens,
+                ),
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_webview_auth
 import device_info_plus
+import firebase_auth
+import firebase_core
+import google_sign_in_ios
 import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
